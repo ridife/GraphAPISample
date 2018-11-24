fc07c82e253c801725a0af5ff23a33bc689b27d8
Ridi Fe <ridi@mvps.org>
2018-11-24 17:27:25 +0700

    initial commit

---
+++ WindowsFormsClientCalendar/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsClientCalendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddCalendar = new System.Windows.Forms.Button();
+            this.graphLoginComponent1 = new Microsoft.Toolkit.Services.WinForms.GraphLoginComponent(this.components);
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(63, 40);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(108, 39);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_ClickAsync);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(45, 17);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Nama";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(63, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(340, 22);
+            this.txtName.TabIndex = 3;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(63, 140);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(340, 22);
+            this.dtpDate.TabIndex = 4;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(63, 112);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(340, 22);
+            this.txtTitle.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Remind Client ";
+            // 
+            // btnAddCalendar
+            // 
+            this.btnAddCalendar.Location = new System.Drawing.Point(63, 168);
+            this.btnAddCalendar.Name = "btnAddCalendar";
+            this.btnAddCalendar.Size = new System.Drawing.Size(108, 39);
+            this.btnAddCalendar.TabIndex = 7;
+            this.btnAddCalendar.Text = "Tambah Kalendar";
+            this.btnAddCalendar.UseVisualStyleBackColor = true;
+            this.btnAddCalendar.Click += new System.EventHandler(this.btnAddCalendar_Click);
+            // 
+            // graphLoginComponent1
+            // 
+            this.graphLoginComponent1.ClientId = null;
+            this.graphLoginComponent1.Scopes = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 217);
+            this.Controls.Add(this.btnAddCalendar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddCalendar;
+        private Microsoft.Toolkit.Services.WinForms.GraphLoginComponent graphLoginComponent1;
+    }
+}
+
